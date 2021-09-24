30016d916164c4e4c969e62cede6997a381bdd53
Marvin <=>
2021-09-24 13:46:58 -0700

    Contains the layout of the code all in comments, no code can be found in this version

---
+++ RestaurantBill.c
@@ -0,0 +1,11 @@
+//create a totalCost variable to hold the cost of the meal
+//create a userReportedTax variable to store the tax percentage of the area as reported by the user
+//create a userReportedTip variable to store the percentage of the tip value as reported by the user 
+
+//ask the user for the cost of the meal and store it in that totalCost variable 
+//ask the user for the tax in their area and store it in the userReportedTax variable
+//as kthe user for the tip percentage of the area and store it in the userReportedTip variable 
+
+//print out the cost of the meal
+//print out the calculated tax 
+//print out the calculated tip
